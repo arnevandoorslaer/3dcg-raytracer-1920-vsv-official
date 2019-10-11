390f872849e5446b8011f0a96cda86a453619416
lemonade <32491056+lem-onade@users.noreply.github.com>
2019-10-11 13:20:54 +0200

    Update ray-tracers.h

--- raytracer/raytracer/raytracers/ray-tracers.h
+++ raytracer/raytracer/raytracers/ray-tracers.h
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// Add an include for each ray tracer
-#include "raytracers/ray-tracer-v0.h"
-=======
 // Add an include for each ray tracer
 #include "raytracers/ray-tracer-v0.h"
 #include "raytracers/ray-tracer-v1.h"
->>>>>>> 4db212046ea602160826fc094bd37824626cfaea
