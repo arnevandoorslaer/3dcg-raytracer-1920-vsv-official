97e9f1bd77bddde8604ef9f100a2e80f4c7bd986
BelgianNoise <king.arthur360@gmail.com>
2019-10-18 20:18:58 +0200

    team data was kapot

--- checker/team-data.rb
+++ checker/team-data.rb
@@ -1,125 +1,3 @@
-<<<<<<< HEAD
-team_member 'Arthur' do
-  extension BasicSample
-  extension BasicScripting
-end
-
-team_member 'Arne' do
-  extension RandomSampler
-end
-
-team_member 'Stijn' do
-  extension MaterialVerticalLines
-  extension PlaneXZ
-end
-
-
-
-# Possible extensions
-# You can copy paste lines below
-
-# extension BasicSample
-# extension BasicScripting
-# extension RayTracerV1
-# extension RayTracerV2
-# extension RayTracerV3
-# extension RayTracerV4
-# extension RayTracerV5
-# extension RayTracerV6
-# extension RandomSampler
-# extension StratifiedSampler
-# extension JitteredSampler
-# extension HalfJitteredSampler
-# extension NRooksSampler
-# extension MultijitteredSampler
-# extension DepthOfFieldCamera
-# extension FisheyeCamera
-# extension OrthographicCamera
-# extension DirectionalLight
-# extension SpotLight
-# extension AreaLight
-# extension Voronoi2D
-# extension Voronoi3D
-# extension MaterialDalmatian2D
-# extension MaterialVerticalLines
-# extension MaterialGrid2d
-# extension MaterialCheckered2D
-# extension MaterialDalmatian3D
-# extension MaterialGrid3d
-# extension MaterialCheckered3D
-# extension MaterialWorley2D
-# extension MaterialWorley3D
-# extension MaterialPerlin2D
-# extension MaterialPerlin3D
-# extension MaterialMarble2D
-# extension MaterialMarble3D
-# extension MaterialTransformer2D
-# extension MaterialTransformer3D
-# extension MaterialScale2D
-# extension MaterialScale3D
-# extension MaterialTranslate2D
-# extension MaterialTranslate3D
-# extension MaterialRotate2D
-# extension MaterialRotate3D
-# extension PlaneXZ
-# extension PlaneYZ
-# extension PlaneXZOptimized
-# extension PlaneYZOptimized
-# extension ConeAlongX
-# extension ConeAlongY
-# extension ConeAlongZ
-# extension ConeAlongXOptimized
-# extension ConeAlongYOptimized
-# extension ConeAlongZOptimized
-# extension SquareXY
-# extension SquareXZ
-# extension SquareYZ
-# extension SquareXYOptimized
-# extension SquareXZOptimized
-# extension SquareYZOptimized
-# extension Cube
-# extension CylinderAlongX
-# extension CylinderAlongY
-# extension CylinderAlongZ
-# extension CylinderAlongXOptimized
-# extension CylinderAlongYOptimized
-# extension CylinderAlongZOptimized
-# extension Triangle
-# extension TriangleOptimized
-# extension BoundingBox
-# extension Cropper
-# extension CropperOptimized
-# extension Bumpifier
-# extension Intersection
-# extension IntersectionOptimized
-# extension Difference
-# extension DifferenceOptimized
-# extension PrimitiveScaling
-# extension PrimitiveRotationX
-# extension PrimitiveRotationY
-# extension PrimitiveRotationZ
-# extension Group
-# extension Edge
-# extension Cartoon
-# extension ParallelScheduler
-# extension MotionBlur
-# extension Bmp
-# extension Ppm
-# extension EasingLibrary
-# extension Bounce
-# extension Elastic
-# extension Quadratic
-# extension Cubic
-# extension Quintic
-# extension PointAnimation
-# extension AngleAnimation
-# extension Quaternions
-# extension CircularAnimation
-# extension Lissajous
-# extension Cyclic
-# extension Slicer
-# extension Mesh
-=======
 
 team_member 'Arthur' do
   extension BasicSample
@@ -129,27 +7,29 @@ team_member 'Arthur' do
   extension RayTracerV2
   extension RayTracerV3
   extension RayTracerV4
+
+  extension ParallelScheduler
 end
 
 team_member 'Arne' do
   extension BasicSample
   extension BasicScripting
+
+  extension RandomSampler
 end
 
 team_member 'Stijn' do
   extension BasicSample
   extension BasicScripting
 
+  extension MaterialVerticalLines
   extension PlaneXZ
 end
-
-
 
 # Possible extensions
 
 # extension RayTracerV5
 # extension RayTracerV6
-# extension RandomSampler
 # extension StratifiedSampler
 # extension JitteredSampler
 # extension HalfJitteredSampler
@@ -224,7 +104,6 @@ end
 # extension Group
 # extension Edge
 # extension Cartoon
-# extension ParallelScheduler
 # extension MotionBlur
 # extension Bmp
 # extension Ppm
@@ -242,5 +121,3 @@ end
 # extension Cyclic
 # extension Slicer
 # extension Mesh
-
->>>>>>> 80834229f8f514c49998ff92bd1f3f29a5eee870
