c7fe9f37c8b7fb6900096fff150a5d02bfe9d83f
Stijn Taelemans <taelemansst@gmail.com>
2020-01-14 09:36:45 +0100

    stijn mesh done

--- mesh stijn/functions.rb
+++ mesh stijn/functions.rb
@@ -1,13 +1,23 @@
 require_relative "triangle"
 
 def lowest(attr, triangles)
-    triangles.map{ |triangle| triangle.send("min", attr) }.min
+    triangles.map{ |triangle| triangle.min(attr) }.min
 end
 
 def highest(attr, triangles)
-    triangles.map{ |triangle| triangle.send("max", attr) }.min
+    triangles.map{ |triangle| triangle.max(attr) }.max
 end
 
 def in_box(attr, triangle, min, max)
     (triangle.min(attr) > min) && (triangle.max(attr) < max)
-end     
+end
+
+def x_in_box(triangle, min, max)
+    (triangle.min("x") > min) && (triangle.max("x") < max)
+end 
+def in_box(attr, triangle, min, max)
+    (triangle.min(attr) > min) && (triangle.max(attr) < max)
+end 
+def in_box(attr, triangle, min, max)
+    (triangle.min(attr) > min) && (triangle.max(attr) < max)
+end 
--- mesh stijn/mesh.rb
+++ mesh stijn/mesh.rb
@@ -1,8 +1,9 @@
 require_relative "triangle"
 require_relative "functions"
 
-meshfile = File.readlines("temp.mesh", "r")[0].split("\n")
-output = "output.txt"
+infile = "bunny.mesh"
+meshfile = File.readlines(infile, "r")[0].split("\n")
+output = infile.concat(".out.txt")
 @result = ""
 
 no_of_points = meshfile.slice!(0)
@@ -23,7 +24,6 @@ end
 
 def create_box_structure(triangles)
     if triangles.length() > 2
-        puts "inside first if"
         widths = {
             x_width: highest("x", triangles) - lowest("x", triangles),
             y_width: highest("y", triangles) - lowest("y", triangles),
@@ -33,24 +33,16 @@ def create_box_structure(triangles)
         first = []
         second = []
 
-        puts widths.max_by{|k,v| v}[0].to_s()
-        puts widths[:x_width].class
-
         if widths.values().max == widths[:x_width]
             min_x = lowest("x", triangles)
             center_x = min_x + (widths[:x_width] / 2)
-            puts "#{min_x} #{center_x}"
-            puts triangles
             for triangle in triangles
-                in_box("x", triangle, min_x, center_x) ? first << triangle : second << triangle
+                x_in_box(triangle, min_x, center_x) ? first << triangle : second << triangle
             end
-            puts first
-            puts second
 
         elsif widths.values().max == widths[:y_width]
             min_y = lowest("y", triangles)
             center_y = min_y + (widths[:y_width] / 2)
-            puts "#{min_x} #{center_x}"
             for triangle in triangles
                 in_box("y", triangle, min_y, center_y) ? first << triangle : second << triangle
             end
@@ -58,14 +50,14 @@ def create_box_structure(triangles)
         elsif widths.values().max == widths[:z_width]
             min_z = lowest("z", triangles)
             center_z = min_z + (widths[:z_width] / 2)
-            puts "#{min_x} #{center_x}"
             for triangle in triangles
                 in_box("z", triangle, min_z, center_z) ? first << triangle : second << triangle
             end
         end
 
         if first.length() == 0
-            for i in 0..(second.length.to_f() / 2).ceil
+            range = (second.length().to_f() / 2).floor
+            for i in 1..range
                 first << second.slice!(0)
             end
         end
@@ -75,16 +67,14 @@ def create_box_structure(triangles)
         @result.concat("box 2\n")
 
     elsif triangles.length() > 0
-        puts "inside second if"
         for triangle in triangles do
             @result.concat(triangle.to_s())
         end
-        @result.concat("box #{triangles.length()}")
+        @result.concat("box #{triangles.length()}\n")
     end
 end
 
 create_box_structure(triangles)
 @result.concat("stop")
-puts @result
 File.open(output, "w") { |f| f.write @result}
 puts "mesh file succesfully written to #{output}"
