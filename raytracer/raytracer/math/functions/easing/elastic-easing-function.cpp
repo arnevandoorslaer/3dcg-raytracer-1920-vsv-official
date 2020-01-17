0bbc680f7bf5d9e6682daf56f778667f3432fd20
BelgianNoise <king.arthur360@gmail.com>
2020-01-17 01:17:48 +0100

    elastic

--- raytracer/raytracer/math/functions/easing/elastic-easing-function.cpp
+++ raytracer/raytracer/math/functions/easing/elastic-easing-function.cpp
@@ -10,13 +10,11 @@ EasingFunction math::functions::easing::elastic(int b, int a) {
 
 	std::function<double(double)> lambda = [a, b](double t) {
 		assert(interval(0.0, 1.0).contains(t));
-		if (a == 0)
-		{
-			return 1 - exp(-(1 / 0.00000001) * t) * (cos(b * t + 1));
+		if (a == 0) {
+			return 1 - pow(2, -1 / 0.5 * t) * cos(t * M_PI * b/3);
 		}
-		else
-		{
-			return 1 - exp(-(1 / a) * t) * (cos(b * t + 1));
+		else {
+			return 1 - pow(2, -1 / a * t) * cos(t * M_PI * b/3);
 		}
 	};
 
